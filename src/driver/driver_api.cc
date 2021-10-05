2abb2ceff667eb472fc6d06e49cb132ce581337a
Valery Chernov <valery.chernov@deelvin.com>
2021-10-05 16:16:35 +0300

    remove native api for onnxruntime

--- src/driver/driver_api.cc
+++ src/driver/driver_api.cc
@@ -23,7 +23,6 @@
  */
 #include <dmlc/thread_local.h>
 #include <tvm/driver/driver_api.h>
-#include <tvm/driver/jit_interface.h>
 #include <tvm/ir/transform.h>
 #include <tvm/runtime/c_runtime_api.h>
 #include <tvm/runtime/registry.h>
@@ -542,55 +541,3 @@ runtime::Module build(const IRModule& funcs, const Target& target_arg,
 }
 
 }  // namespace tvm
-
-tvm::runtime::Module TVMCompile(const std::string& onnx_txt, const std::string& target, const std::string& target_host, int opt_level, const std::vector<std::vector<int64_t>>& input_shapes)
-{
-  tvm::Array<tvm::Array<tvm::Integer>> shapes;
-  for (size_t i = 0; i < input_shapes.size(); i++)
-  {
-    tvm::Array<tvm::Integer> shape;
-    for (auto& dim : input_shapes[i])
-    {
-      shape.push_back(tvm::Integer(dim));
-    }
-    shapes.push_back(shape);
-  }
-
-  const tvm::PackedFunc* compile = tvm::runtime::Registry::Get("tvm_onnx_import_and_compile");
-  tvm::runtime::Module mod = (*compile)(TVMByteArray{onnx_txt.data(), onnx_txt.size()}, target, target_host, opt_level, shapes);
-  return mod;
-}
-
-void TVMExtractOutputShapes(tvm::runtime::Module& mod, size_t num_outputs, std::vector<std::vector<int64_t>>& output_shapes)
-{
-  tvm::PackedFunc get_output = mod.GetFunction("get_output", false);
-  for (size_t i = 0; i < num_outputs; i++)
-  {
-    tvm::runtime::NDArray output_array = get_output(i);
-    const auto& shape = output_array.Shape();
-    std::vector<int64_t> oshape;
-    for (const auto& dim : shape)
-    {
-      oshape.push_back(dim);
-    }
-    output_shapes.push_back(oshape);
-  }
-}
-
-void TVMRun(tvm::runtime::Module& mod, std::vector<DLTensor>& inputs, std::vector<DLTensor>& outputs, tvm::runtime::TVMRetValue *ret)
-{
-  tvm::PackedFunc set_input = mod.GetFunction("set_input_zero_copy", false);
-  for (size_t i = 0; i < inputs.size(); i++)
-  {
-    set_input(i, &inputs[i]);
-  }
-
-  const tvm::PackedFunc* run = tvm::runtime::Registry::Get("tvm_run_with_benchmark");
-  (*run)(mod);
-
-  tvm::PackedFunc get_output = mod.GetFunction("get_output", false);
-  for (size_t i = 0; i < outputs.size(); i++)
-  {
-    get_output(i, &outputs[i]);
-  }
-}
