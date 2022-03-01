6e6f548ffd3b140a0c52a4b33f410be59e966744
Sergey Shtin <sergey.shtin@deelvin.com>
2022-03-01 18:10:26 +0300

    some small updates

--- demo/DLRM_mi/run_resnet50.sh
+++ demo/DLRM_mi/run_resnet50.sh
@@ -3,19 +3,30 @@ tuning="tuning"
 mkdir -p "./${tuning}"
 pth_twm="${PWD}/../../"
 length_thr=$(nproc)
-length_thr=$((length_thr+1))
-
+length_thr=$((length_thr + 1))
+echo $length_thr
 export TVM_HOME=$pth_twm
 export PYTHONPATH=$TVM_HOME/python:${PYTHONPATH}
 i=1
-export OMP_NUM_THREADS=8
+# export OMP_NUM_THREADS=8
 model_pth="/home/ubuntu/dev/tvm/demo"
 # python ./tune_resnet50.py --onnx-model ${model_pth}/resnet50_v1.onnx --output-log ./resnet50.json --output-folder ./out
 # python ./compile.py --model-path ${model_pth}/resnet50_v1.onnx --model-name resnet50 --tuning-log-file ./resnet50.json --output-name resnet50 --batch-size 1
 for (( i = 1; i < length_thr; i++ )); do
-  for (( j = 1; j < length_thr; j++ )); do
-    fname="${tuning}/log_RESNET50_i${i}_t${j}.txt"
+  threads=$(($(nproc)/i))
+  threads=$((threads + 1))
+  for (( j = 1; j < threads; j++ )); do
+    fname="${tuning}/log_RESNET50_v1_i${i}_t${j}.txt"
     echo ${fname} ${i} ${j}
-    python ./multi_instance.py --model-path ./__prebuilt/resnet50_b1/resnet50_b1.so --model-name resnet50 --num-instances ${i} --num-threads ${j} > ${fname}
+    python ./multi_instance.py --model-path ./__prebuilt/resnet_b1_avx2/resnet50_b1_avx2.so --model-name resnet --num-instances ${i} --num-threads ${j} > ${fname}
+  done
+done
+upper=$(nproc)
+upper=$((upper + 1))
+for (( i = length_thr; i < upper; i++ )); do
+  for (( j = 1; j < 3; j++ )); do
+    fname="${tuning}/log_RESNET50_v1_i${i}_t${j}.txt"
+    echo ${fname} ${i} ${j}
+    python ./multi_instance.py --model-path ./__prebuilt/resnet_b1_avx2/resnet50_b1_avx2.so --model-name resnet --num-instances ${i} --num-threads ${j} > ${fname}
   done
 done
--- demo/bert/run_all_bert.sh
+++ demo/bert/run_all_bert.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
-instances=(1 2 4 8 10 12 14 16)
-threads=(1 2 4 8 10 12 14 16)
-length=${#instances[@]}
-length_thr=${#threads[@]}
+# instances=(1 2 4 8 10 12 14 16)
+# threads=(1 2 4 8 10 12 14 16)
+# length=${#instances[@]}
+# length_thr=${#threads[@]}
 tuning="tuning"
 mkdir -p "./${tuning}"
 pth_tvm="${PWD}/../../"
-# procs=$(nproc)
-# procs_2=$((procs/2))
+length_thr=$(nproc)
+length_thr=$((length_thr + 1))
+
+procs=$(nproc)
+procs_2=$((procs/2))
 
 export TVM_HOME=$pth_tvm
 export PYTHONPATH=$TVM_HOME/python:${PYTHONPATH}
-export OMP_NUM_THREADS=8
+# export OMP_NUM_THREADS=8
 
 if [ ! -d "./out" ] 
 then
     mkdir ./out
 fi
 
-python ./tune_bert.py --output-log ./test_log.json --output-folder ./out
-python compile.py --tuning-log-file ./test_log.json --output-name ./out/test_bert --batch-size 1 --onnx-model ../inference/language/bert/build/data/bert_tf_v1_1_large_fp32_384_v2/model.onnx
-for (( i = 0; i < length; i++ )); do
-  for (( j = 0; j < length_thr; j++ )); do
-    fname="${tuning}/bert_log_i${instances[$i]}_t${threads[$j]}.txt"
-    echo ${fname} ${instances[$i]} ${threads[$j]}
-    python ./multi_instance_BERT.py --model ./out/test_bert_b1.so --num-instances ${instances[$i]} --num-threads ${threads[$j]} > ${fname}
+# python ./tune_bert.py --output-log ./test_log.json --output-folder ./out
+# python compile.py --tuning-log-file ./test_log.json --output-name ./out/test_bert --batch-size 1 --onnx-model ../inference/language/bert/build/data/bert_tf_v1_1_large_fp32_384_v2/model.onnx
+
+for (( i = 1; i < length_thr; i++ )); do
+  threads=$(($(nproc)/i))
+  threads=$((threads + 1))
+  for (( j = 1; j < threads; j++ )); do
+    fname="${tuning}/bert_log_v1_i${i}_t${j}.txt"
+    echo ${fname} ${i} ${j}
+    python ./multi_instance_BERT.py --model ./out/test_bert_b1.so --num-instances ${i} --num-threads ${j} > ${fname}
+  done
+done
+
+for (( i = procs_2; i < length_thr; i++ )); do
+  for (( j = 2; j < 3; j++ )); do
+    fname="${tuning}/bert_log_v1_i${i}_t${j}.txt"
+    echo ${fname} ${i} ${j}
+    python ./multi_instance_BERT.py --model ./out/test_bert_b1.so --num-instances ${i} --num-threads ${j} > ${fname}
   done
 done
