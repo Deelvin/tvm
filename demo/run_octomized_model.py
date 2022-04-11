ae1815a7e508f88a5815fafcc45f843ad6ff4746
Sergey Shtin <sergey.shtin@deelvin.com>
2022-04-11 19:46:37 +0300

    debug: script update

--- demo/run_octomized_model.py
+++ demo/run_octomized_model.py
@@ -1,4 +1,5 @@
 from ast import arg
+from email.policy import default
 import octomizer
 import requests
 import json
@@ -7,10 +8,58 @@ from datetime import *
 import argparse
 import matplotlib.pyplot as plt
 import octomizer.client as octoclient
+import octomizer.project as project
+import octomizer.models.onnx_model as onnx_model
 import octomizer.package_type
 from octoml.octomizer.v1.workflows_pb2 import WorkflowStatus
+from octomizer.model_variant import AutoschedulerOptions
 from subprocess import Popen, PIPE
+from DLRM_mi.models import get_host_target
+import onnx
 
+PROJECT_NAME = "multi_instance_check"
+DESCRIPTION = "Tuning testing project to analize system performance."
+
+#other types tbd
+scalar_type_to_tvm_type = {
+     1 : "float32",         # 1
+     7 : "int64",       # 7
+}
+
+def get_input(model, batch_size, model_name):
+  retval = {}
+  shape_dtypes = {}
+  for input in model.graph.input:
+    nm = input.name
+    shape = []
+    # # get type of input tensor
+    tensor_type = input.type.tensor_type
+    # check if it has a shape:
+    if tensor_type.HasField("shape"):
+      for d in tensor_type.shape.dim:
+        if d.HasField("dim_value"): # known dimension
+          shape.append(int(d.dim_value))
+        elif d.HasField("dim_param"): # unknown dimension with symbolic name
+          # workaround for now!
+          shape.append(batch_size)
+    # print(tensor_type)
+    dtype = "float32"
+    if tensor_type.HasField('elem_type'):
+      # print(sym_help.cast_pytorch_to_onnx.keys())
+      if not tensor_type.elem_type in scalar_type_to_tvm_type.keys():
+        print("ERROR: unknown onnx dtype : ", tensor_type.elem_type)
+        exit(0)
+      dtype = scalar_type_to_tvm_type[tensor_type.elem_type]
+      # print(tensor_type.elem_type)
+    retval[nm] = shape
+    shape_dtypes[nm] = dtype
+  # workaround because DLRM does not have dynamic batch
+  if model_name == "dlrm":
+    retval["input.1"][0] = batch_size
+    retval["lS_o"][1] = batch_size
+    retval["lS_i"][1] = batch_size
+
+  return retval, shape_dtypes
 
 def download_tuning_records(workflow, download_path=None):
     assert workflow.proto.status.state == WorkflowStatus.COMPLETED, f"workflow status must be COMPLETED, found {workflow.proto.status}"
@@ -30,7 +79,9 @@ def download_tuning_records(workflow, download_path=None):
     raw_log = "[" + ( ",\n".join(raw_log.split("\n")))[:-2] + "]"
     return json.loads(raw_log)
 
-def download_model(model_uuid, system_name):
+def download_model(client, model_uuid, system_name):
+  print(dir(client))
+  exit(0)
   p = Popen(['octomizer', '--json', 'list-workflows', model_uuid], stdin=PIPE, stdout=PIPE, stderr=PIPE)
   output, err = p.communicate()
   if len(output) == 0 or p.returncode != 0:
@@ -42,6 +93,7 @@ def download_model(model_uuid, system_name):
   data = json.loads(json_decoded)
   time_val = None
   data_to_extract = {}
+
   for i in data:
     # search for the latest Octomizer result
     spec = i['hardware']['platform']
@@ -75,6 +127,44 @@ def download_model(model_uuid, system_name):
     print(lib)
     if os.path.isfile(lib):
       return lib
+  return None
+
+def tune_model_by_octomizer(client, model_path, batch_size, model_name, system_name):
+  if not os.path.exists(model_path):
+    print("ERROR: model {} does not exist.".format(model_path))
+    return False
+  # parse model inputs
+
+  model = onnx.load(model_path)
+  inpt_shapes, inpt_types = get_input(model, batch_size, model_name)
+  del model
+
+  octo_project = project.Project(
+        client,
+        name=PROJECT_NAME,
+        description=DESCRIPTION,
+    )
+  model_package_name = "{}_octomized".format(os.path.basename(model_path))
+  print(model_package_name)
+
+  model = onnx_model.ONNXModel(
+        client,
+        name=model_package_name,
+        model=model_path,
+        description=DESCRIPTION,
+        project=octo_project,
+    )
+  model_variant = model.get_uploaded_model_variant()
+  octomize_workflow = model_variant.octomize(
+        system_name,
+        tuning_options=AutoschedulerOptions(
+            trials_per_kernel=3, early_stopping_threshold=1
+        ),
+        tvm_num_threads=os.cpu_count()//2,
+        input_shapes=inpt_shapes,
+        input_dtypes=inpt_types
+    )
+  print(octomize_workflow.uuid)
   return None
 
 def parse_output(lines):
@@ -141,13 +231,13 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("--token", required=False, help="Octomizer API token", default="default")
   parser.add_argument("--model-uuid", required=False, help="required model from OctoML.ai", default="default")
-  parser.add_argument("--system-name", required=True, help="system name which should be used for data extraction", default="default")
+  parser.add_argument("--platform", required=True, help="platform which should be used for data extraction", default="default")
   parser.add_argument("--download-log", required=False, help="workload uuid", default="default")
   parser.add_argument("--model-name", required=True, help="model name for inference [bert, resnet, dlrm]", default="default")
-  parser.add_argument("--model-path", required=False, help="model path if log fiel is used for analysis", default="default")
+  parser.add_argument("--model-path", required=False, help="model path if log file is used for analysis", default="default")
   parser.add_argument("--batch-size", required=False, help="batch size definition for inference, default value is 1", default=1, type=int)
   parser.add_argument("--trial-time", required=False, help="inference time definition in sec, default value is 10", default=10, type=int)
-  
+
   args = parser.parse_args()
   model_name = args.model_name
   model_path = args.model_path
@@ -157,11 +247,32 @@ if __name__ == "__main__":
   if args.token != "default":
     os.environ['OCTOMIZER_API_TOKEN'] = args.token
   client = octoclient.OctomizerClient()
+  targets = client.get_hardware_targets()
+  platforms = [x.platform for x in targets]
+  if not args.platform in platforms:
+    print("EERROR: platform is not defined.")
+    print("Please select platform parameter from the list:")
+    for elem in platforms:
+      print(elem)
+    exit(0)
   curr_user = client.get_current_user()
-  file_path = os.path.dirname(os.path.realpath(__file__))
+  print(curr_user)
+  # for mod in models:
+  #   print(dir(mod))
+  #   print(mod.proto.name)
+  #   print(mod.proto.owned_by)
+  #   exit(0)
 
+  file_path = os.path.dirname(os.path.realpath(__file__))
   if args.download_log == 'default':
-    model_lib_path = os.path.join(file_path, download_model(args.model_uuid, args.system_name))
+    if args.model_uuid != 'default':
+      models = client.list_models()
+      curr_user_models = [mod.proto for mod in models if mod.proto.created_by == curr_user.uuid and args.model_uuid == mod.proto.uuid]
+      print(curr_user_models)
+      model_lib_path = os.path.join(file_path, download_model(client, args.model_uuid, args.platform))
+    else:
+      print("here")
+      model_lib_path = tune_model_by_octomizer(client, args.model_path, args.batch_size, args.model_name, args.platform)
     os.chdir(os.path.join(file_path, 'DLRM_mi'))
     if model_lib_path != None:
       # just run model
@@ -196,6 +307,6 @@ if __name__ == "__main__":
   output, err = p.communicate()
   os.chdir(file_path)
   if len(output) != 0:
-    draw_legend(output, args.system_name, model_name)
+    draw_legend(output, args.platform, model_name)
   else:
     print("ERROR: empty inference output.")
